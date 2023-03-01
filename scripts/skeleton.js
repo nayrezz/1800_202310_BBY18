872072a5e2e9b42c48f4f7445dd1a3f0dd9426dc
Kari Sturgeon <sturgeonkari@gmail.com>
2023-02-28 20:35:53 -0800

    linked the pages we have to the navbar

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -3,7 +3,18 @@
 // (navbar, footer, and other things) into html doc. 
 //---------------------------------------------------
 function loadSkeleton(){
-    console.log($('#navbarPlaceholder').load('./text/navOriginal.html'));
-    console.log($('#footerPlaceholder').load('./text/footer.html'));
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            // Do something for the user here.
+            console.log($('#navbarPlaceholder').load('./text/navOriginal.html'));
+            console.log($('#footerPlaceholder').load('./text/footer.html'));
+        } else {
+            // No user is signed in.
+            console.log($('#navbarPlaceholder').load('./text/navOriginal.html'));
+            console.log($('#footerPlaceholder').load('./text/footer.html'));
+        }
+    });
 }
+
 loadSkeleton();  //invoke the function
